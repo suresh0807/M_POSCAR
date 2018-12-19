25f32db8c8ea6496ef4c3e6aa534a5843913a978
Suresh Kondati Natarajan <suresh0807@gmail.com>
2018-12-19 14:33:50 +0000

    Update rePOS.sh

--- rePOS.sh
+++ rePOS.sh
@@ -1,24 +1,69 @@
-cp POSCAR POSCAR-backup
-a=`echo "$1"`
-poscar2runner.sh < POSCAR > input.data
-head -n 4 input.data > header
-tail -n 3 input.data > footer
-for((i=2;i<=$a+1;i++))
+#POSCAR must have elements in the 1st line
+#POSCAR must have number of atoms of elements in the 6th line
+#usage ./rePOS.sh 4 W Ru N P
+
+cp POSCAR POSCAR_backup
+nelt=`head -n 1 POSCAR | awk '{print NF}'`
+
+seldyn=`grep 'Selective dynamics' POSCAR_backup`
+cart=`grep 'Cartesian' POSCAR_backup`
+dirt=`grep 'Direct' POSCAR_backup`
+
+echo "save elts $nelt"
+
+for((i=1;i<=$nelt;i++))
 do
-grep "${!i} " input.data > $i
+	A[$i]=`awk -v ii=$i 'NR==1{print $ii}' POSCAR_backup`
+	n[$i]=`awk -v ii=$i 'NR==6{print $ii}' POSCAR_backup`
+	echo "${A[$i]} ${n[$i]}"
 done
 
-cat header > input.new
+echo " save cart"
+if [ "$cart" == 'Cartesian' ]
+then
+	sed -e '1,/Cartesian/d' POSCAR_backup > cart
+else
+	sed -e '1,/Direct/d' POSCAR_backup > cart
+fi
 
-for((i=2;i<=$a+1;i++))
+for((i=1;i<=$nelt;i++))
 do
-cat $i >> input.new
-rm $i
+	head -n ${n[$i]} cart >> ${A[$i]}.cart
+	sed -i 1,${n[$i]}d cart
 done
 
-cat footer >> input.new
+echo "write POSCAR"
+rm POSCAR
+for((i=2;i<=$1+1;i++))
+do
+	printf "${!i} " >> POSCAR
+done
+
+printf "\n" >> POSCAR
+head -n 5 POSCAR_backup | tail -n 4 >> POSCAR
+
+for((i=2;i<=$1+1;i++))
+do
+	numi=`wc -l < ${!i}.cart`
+	printf "$numi " >> POSCAR  
+done
+printf "\n" >> POSCAR
+if [ "$seldyn" == 'Selective dynamics' ]
+then
+	printf "Selective Dynamics \n" >> POSCAR
+fi
+
+if [ "$cart" == 'Cartesian' ]
+then
+	printf "Cartesian \n" >> POSCAR
+else
+	printf "Direct \n" >> POSCAR
+fi
+
+for((i=2;i<=$1+1;i++))
+do
+	cat ${!i}.cart >> POSCAR
+done
 
 
-runner2poscar.bash input.new > POSCAR
-
-rm input.data input.new header footer
+rm cart *.cart
