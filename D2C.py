b6b8c0639404cb63d6bd3b6453d857bb4976cff7
Suresh Kondati Natarajan <suresh0807@gmail.com>
2019-04-12 11:29:05 +0100

    Update D2C.py

--- D2C.py
+++ D2C.py
@@ -1,5 +1,6 @@
-#with ase installed, use 'python D2C.py' in the folder with POSCAR to convert the direct coordinated to cartesian. It is similar to opening the POSCAR in ASE and saving it.
+#with ase installed, use 'python D2C.py XXX' to convert direct coordinated XXX to cartesian. It is similar to opening the POSCAR in ASE and saving it.
 from ase import Atoms
 from ase.io import read,write
-a = read('POSCAR')
+print ("converting ",sys.argv[1]," to POSCAR")
+a = read(sys.argv[1])
 write('POSCAR', a)
