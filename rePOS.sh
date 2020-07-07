dbb04ca54850f0440f7f2be2f4cd065b3f69f272
Suresh Kondati Natarajan <suresh0807@gmail.com>
2020-07-07 17:25:29 +0300

    Update rePOS.sh

--- rePOS.sh
+++ rePOS.sh
@@ -13,30 +13,32 @@ echo "save elts $nelt"
 
 for((i=1;i<=$nelt;i++))
 do
-	A[$i]=`awk -v ii=$i 'NR==1{print $ii}' POSCAR_backup`
-	n[$i]=`awk -v ii=$i 'NR==6{print $ii}' POSCAR_backup`
-	echo "${A[$i]} ${n[$i]}"
+        A[$i]=`awk -v ii=$i 'NR==1{print $ii}' POSCAR_backup`
+        n[$i]=`awk -v ii=$i 'NR==6{print $ii}' POSCAR_backup`
+        echo "${A[$i]} ${n[$i]}"
 done
 
-echo " save cart"
-if [ "$cart" == 'Cartesian' ]
+echo "save cart"
+if [ $cart == "Cartesian" ]
 then
-	sed -e '1,/Cartesian/d' POSCAR_backup > cart
+        sed -e '1,/Cartesian/d' POSCAR_backup > cart
 else
-	sed -e '1,/Direct/d' POSCAR_backup > cart
+        sed -e '1,/Direct/d' POSCAR_backup > cart
 fi
 
 for((i=1;i<=$nelt;i++))
 do
-	head -n ${n[$i]} cart >> ${A[$i]}.cart
-	sed -i 1,${n[$i]}d cart
+        head -n ${n[$i]} cart >> ${A[$i]}.cart
+        sed -i 1,${n[$i]}d cart
 done
+
+
 
 echo "write POSCAR"
 rm POSCAR
 for((i=2;i<=$1+1;i++))
 do
-	printf "${!i} " >> POSCAR
+        printf "${!i} " >> POSCAR
 done
 
 printf "\n" >> POSCAR
@@ -44,25 +46,25 @@ head -n 5 POSCAR_backup | tail -n 4 >> POSCAR
 
 for((i=2;i<=$1+1;i++))
 do
-	numi=`wc -l < ${!i}.cart`
-	printf "$numi " >> POSCAR  
+        numi=`wc -l < ${!i}.cart`
+        printf "$numi " >> POSCAR  
 done
 printf "\n" >> POSCAR
-if [ "$seldyn" == 'Selective dynamics' ]
+if [ $seldyn == "Selective dynamics" ]
 then
-	printf "Selective Dynamics \n" >> POSCAR
+        printf "Selective Dynamics \n" >> POSCAR
 fi
 
-if [ "$cart" == 'Cartesian' ]
+if [ $cart == "Cartesian" ]
 then
-	printf "Cartesian \n" >> POSCAR
+        printf "Cartesian \n" >> POSCAR
 else
-	printf "Direct \n" >> POSCAR
+        printf "Direct \n" >> POSCAR
 fi
 
 for((i=2;i<=$1+1;i++))
 do
-	cat ${!i}.cart >> POSCAR
+        cat ${!i}.cart >> POSCAR
 done
 
 
